0c7a9b43b13b9d409d1215e37195b07a29c465e8
Blend Arifaj <blendarifaj@hotmail.com>
2024-12-13 10:11:11 +0100

    groupi 01 - java 12

---
+++ src/group_01/java_12/Detyra1.java
@@ -0,0 +1,41 @@
+package group_01.java_12;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+// Te krijohet gabimi CustomException - ashtu qe kur te shkaktohet ky gabim
+// mesazhi do te ruhet ne fajllin gabimet.txt
+public class Detyra1 {
+    public static void main(String[] args){
+        try{
+            throw new CustomException("Test 1");
+        }catch (CustomException e){
+
+        }
+
+
+        try{
+            throw new CustomException("Test 2");
+        }catch (CustomException e){
+
+        }
+    }
+}
+
+class CustomException extends Exception{
+    private final static String FILE_NAME = "gabimet.txt";
+    CustomException(String message){
+        super(message);
+        this.handle();
+    }
+    private void handle(){
+        try{
+            FileWriter fw = new FileWriter(FILE_NAME, true);
+            fw.write(this.getMessage() + "\n");
+            fw.close();
+        }catch (IOException e){
+            System.out.println("Gabim gjate ruajtes ne file " + e.getMessage());
+        }
+    }
+}
+
---
+++ src/group_01/java_12/Main.java
@@ -0,0 +1,45 @@
+package group_01.java_12;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        String filename = "src/group_01/java_12/ushtrimet_poo.txt";
+        File file = new File(filename);
+        System.out.println(file.exists());
+        try{
+            if(file.createNewFile()){
+                System.out.println("Keni krijuar fajll-in");
+            }else{
+                System.out.println("Fajll-i ekziston!");
+            }
+        }catch (IOException e){
+            System.out.println("Ka ndodhur nje gabim: " + e.getMessage());
+        }
+//        Scanner
+//        FileWriter
+        try{
+            filename = "src/group_01/Java_12/random.txt";
+            FileWriter fw = new FileWriter(filename, true);
+            fw.write("Writing text in a file!\n");
+            fw.close();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            file = new File(filename);
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
